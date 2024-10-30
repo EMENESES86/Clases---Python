f325e9548260fb4ad6d8ae98f7a8ee88b4172d76
Edison Meneses <efmt86@gmail.com>
2024-10-29 19:29:21 -0500

    sin comentario

--- Práctica en clase/menu.py
+++ Práctica en clase/menu.py
@@ -1,27 +1,27 @@
-# entrada = ""
-# while entrada != "NO":
-#     print("1. SUMA")
-#     print("2. RESTA")
-#     ope = int(input("Que operación va a realizar: "))
-#     if ope>=1 and ope<=2:
-#         num1 = float(input("NUM1: "))
-#         num2 = float(input("NUM2: "))
-#         if ope==1:
-#             suma = num1+num2
-#             print('La suma es:',suma)
-#         elif ope==2:
-#             resta = num1-num2
-#             print('La resta es:',resta)
-#     else:
-#         print("Opción no válida")
-#     entrada = input("Quiere continuar SI/NO: ")
-#     if entrada.upper() == "SI":
-#         pass
-#     elif entrada.upper() == 'NO':
-#         break
-#     else:
-#         print("Opción no válida")
-#         break
+entrada = ""
+while entrada != "NO":
+    print("1. SUMA")
+    print("2. RESTA")
+    ope = int(input("Que operación va a realizar: "))
+    if ope>=1 and ope<=2:
+        num1 = float(input("NUM1: "))
+        num2 = float(input("NUM2: "))
+        if ope==1:
+            suma = num1+num2
+            print('La suma es:',suma)
+        elif ope==2:
+            resta = num1-num2
+            print('La resta es:',resta)
+    else:
+        print("Opción no válida")
+    entrada = input("Quiere continuar SI/NO: ")
+    if entrada.upper() == "SI":
+        pass
+    elif entrada.upper() == 'NO':
+        break
+    else:
+        print("Opción no válida")
+        break
 
 # text = 'Hola mundo'
 # print(text.upper())
